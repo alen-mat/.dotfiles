c9543d8a42142c21fbc0d70c16fae1222e598652
alen-mat <26516117+alen-mat@users.noreply.github.com>
2021-05-23 23:49:26 +0000

    Add : Polybar theme

---
+++ .config/polybar/basic-theme/launch.sh
@@ -0,0 +1,25 @@
+#DIR=`pwd`
+
+DIR="$HOME/.config/polybar/basic-theme"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+#polybar -r -q top -c "$DIR"/config.ini &
+#polybar -r -q bottom -c "$DIR"/config.ini &
+
+echo "-----Top Bar-----" | tee -a /tmp/top-bar-poly.log
+# echo "---Bottom Bar---" | tee -a /tmp/bottom-bar-poly.log
+
+##debugging polybar
+polybar -l info -q top -c "$DIR"/config.ini -r 2>&1 | cat >> /tmp/top-bar-poly.log & disown
+
+#nohup polybar -l info -q bottom -c "$DIR"/config.ini -r > /tmp/bottom-bar-poly.log & 
+
+#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bars launched..."
