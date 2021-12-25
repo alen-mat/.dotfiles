4877351a9ff7d41c97238199f4e3113a4f55371c
alen-mat <26516117+alen-mat@users.noreply.github.com>
2021-12-25 14:28:11 +0530

    Added xmonad theme autogeneration

--- .local/bin/pywal.sh
+++ .local/bin/pywal.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env bash
 
 # Color files
-PFILE="$HOME/.config/polybar/basic-theme/colors.ini"
+PFILE="$HOME/.config/polybar/sleek/colors.ini"
 RFILE="$HOME/.config/rofi/colors.rasi"
 WALL_LOC=$(cat  ~/.config/nitrogen/bg-saved.cfg | grep "file="|cut -d'=' -f 2)
 
 # Get colors
 pywal_get() {
 	wal -i "$1" -q -t
+}
+
+change_xmonad(){
+ XMONAD_MONNET="$HOME/.xmonad/lib/Colors/Monnet.hs"
+ sed -i -e "s/colorBack = .*/colorBack = \"#${BG}\"/g" $XMONAD_MONNET
+ sed -i -e "s/colorFore = .*/colorFore = \"#${FG}\"/g" $XMONAD_MONNET
+ sed -i -e "s/accent = .*/accent = \"${AC}\"/g" $XMONAD_MONNET
+
+ sed -i -e "s/color0 = .*/color0 = \"${color0}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color1 = .*/color1 = \"${color1}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color2 = .*/color2 = \"${color2}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color3 = .*/color3 = \"${color3}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color4 = .*/color4 = \"${color4}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color5 = .*/color5 = \"${color5}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color6 = .*/color6 = \"${color6}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color7 = .*/color7 = \"${color7}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color8 = .*/color8 = \"${color8}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color9 = .*/color9 = \"${color9}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color10 = .*/color10 = \"${color10}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color11 = .*/color11 = \"${color11}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color12 = .*/color12 = \"${color12}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color13 = .*/color13 = \"${color13}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color14 = .*/color14 = \"${color14}\"/g" $XMONAD_MONNET
+ sed -i -e "s/color15 = .*/color15 = \"${color15}\"/g" $XMONAD_MONNET
+
 }
 
 # Change colors
@@ -17,7 +42,8 @@ change_color() {
 	sed -i -e "s/background-alt = #.*/background-alt = #8C${BG}/g" $PFILE
 	sed -i -e "s/foreground = #.*/foreground = #${FG}/g" $PFILE
 	sed -i -e "s/foreground-alt = #.*/foreground-alt = #33${FG}/g" $PFILE
-	sed -i -e "s/primary = #.*/primary = $AC/g" $PFILE
+  sed -i -e "s/accent = #.*/accent = $AC/g" $PFILE
+
 
 	# rofi
 	cat > $RFILE <<- EOF
@@ -34,6 +60,7 @@ change_color() {
 	EOF
 
 	polybar-msg cmd restart
+  change_xmonad
 }
 
 # Main
