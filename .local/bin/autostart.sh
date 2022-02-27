eb6c8350d40726a2a9af5e38fd1016aa97b24c3a
alen-mat <26516117+alen-mat@users.noreply.github.com>
2022-02-27 19:53:36 +0530

    Update : autostart

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env sh
-nitrogen --restore &
 mpd &
 picom &
 [[ $1 = "bar" ]] && ~/.config/polybar/sleek/launch.sh 
 
 deviceid=$(xinput list | grep "Touchpad" | awk '{print $5}'|cut -d= -f2)
 if [[ ${deviceid-} ]];then
-xinput set-prop $deviceid "libinput Middle Emulation Enabled" 1
-xinput set-prop $deviceid "libinput Tapping Enabled" 1
+	device_props=("libinput Middle Emulation Enabled" "libinput Tapping Enabled")
+	for i in ${!device_props[@]}; do
+		xinput list-props $deviceid | grep -q $device_prop && xinput set-prop $deviceid ${!device_props[i]} 1
+	done
 fi
 
-parcellite &
+greenclip daemon
 emacs --bg-daemon &
-#bluetooth 
-flameshot &
+#bluetooth
+#nm-applet 
 pactl load-module module-bluetooth-discover
-~/.local/bin/update-check-job.sh&
 [[ -f ~/.Xresources ]] && xrdb -merge ~/.Xresources
 [[ -f ~/.cache/wal/colors.Xresources ]] && xrdb -merge ~/.cache/wal/colors.Xresources
-bash -c "sleep 5; conky"
+#bash -c "sleep 5; conky"
