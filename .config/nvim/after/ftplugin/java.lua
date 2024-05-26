459494842d67287a52134ce13ebd947dd62f999b
alen-mat <26516117+alen-mat@users.noreply.github.com>
2024-05-26 11:50:19 +0530

    update : folder now submodule

--- .config/nvim/after/ftplugin/java.lua
+++
@@ -1,7 +0,0 @@
-vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
-vim.bo.tabstop = 2
-
-vim.keymap.set('n',"<leader><F5>", ":!zellij run -f -- mvn test<CR>", { desc ='Maven test'})
-
-vim.keymap.set('n','<F5>',  ":!zellij run -f -- mvn clean install<CR>", { desc = 'Maven Build' })
