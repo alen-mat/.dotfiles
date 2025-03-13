603adae6ed526e003f983c707c59ec65f43420af
alen-mat <26516117+alen-mat@users.noreply.github.com>
2025-03-13 13:57:25 +0530

    more ps stuff

--- .config/powershell/psrl.ps1
+++ .config/powershell/psrl.ps1
@@ -3,22 +3,23 @@ Import-Module PSReadLine
 Set-PSReadLineOption -EditMode vi
 Set-PSReadLineOption -ViModeIndicator Cursor
 
-Set-PSReadLineOption -CommandValidationHandler {
-	param($CommadAst)
-	switch ($CommadAst.GetCommandName()){
-		'git'	{
-			$gitcmd = $CommandAst.CommandElements[1].Extent
-			switch ($gitcmd){
-				'cmt'	{
-					[Microsoft.Powershell.PSReadLine]::Replace(
-						$gitcmd.StartOffset,$gitcmd.EndOffset - $gitcmd.StartOffset,'commit')
-				}
-			}
-		}
-	}
-}
-
-Set-PSReadLineKeyHandler -Chord 'Enter' -Function ValidateAndAcceptLine
+Set-PSReadLineKeyHandler -Chord Enter -Function ValidateAndAcceptLine
 Set-PSReadLineKeyHandler -Chord Control+c -Function RevertLine
 
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+Set-PSReadLineOption -CommandValidationHandler {
+    param($CommandAst)
+
+    switch ($CommandAst.GetCommandName()) {
+        'git' {
+            $gitCmd = $CommandAst.CommandElements[1].Extent
+            switch ($gitCmd.Text) {
+                'cmt' {
+                    [Microsoft.PowerShell.PSConsoleReadLine]::Replace(
+                        $gitCmd.StartOffset, $gitCmd.EndOffset - $gitCmd.StartOffset, 'commit')
+                }
+            }
+        }
+    }
+}
