f0841fb77a4ca2bc76f08fccfca6dde2f3bf05ec
alen-mat <26516117+alen-mat@users.noreply.github.com>
2025-04-07 06:37:40 +0530

    awesomewm

--- .config/awesome_min/rc.lua
+++ .config/awesome_min/rc.lua
@@ -3,11 +3,12 @@ pcall(require, "luarocks.loader")
 local awful = require("awful")
 local gfs = require("gears").filesystem
 local beautiful = require("beautiful")
+
 beautiful.xresources.set_dpi(96, 1)
 local w = gfs.get_random_file_from_dir (os.getenv("HOME") .. "/Pictures/wallpapers/", {'jpeg','jpg','svg','png'}, true);
-awful.spawn.easy_async_with_shell(os.getenv("HOME").."/.local/bin/pywal.sh " .. w,function ()
-    
-end )
+
+--- very dangerous
+os.execute(os.getenv("HOME").."/.local/bin/pywal.sh " .. w)
 
 local theme = require("themes.default.theme")
 theme.wallpaper = w
