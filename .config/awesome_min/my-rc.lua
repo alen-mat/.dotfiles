616a091edc5e959e655bf59888d48501327d21e8
alen-mat <26516117+alen-mat@users.noreply.github.com>
2025-06-03 23:34:55 +0530

    awesomewm

--- .config/awesome_min/my-rc.lua
+++ .config/awesome_min/my-rc.lua
@@ -25,7 +25,7 @@ local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
-require("awful.hotkeys_popup.keys")
+--require("awful.hotkeys_popup.keys")
 
 local ss = require('widgets.screenshots')
 
